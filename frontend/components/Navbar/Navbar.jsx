3aee5f1529097862f3997e0e58ce14311ed6344c
paritomarrr <pari2019cse083abesit@gmail.com>
2023-05-11 09:43:36 +0530

    navbar styling

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 // internal imports 
 import Styles from "./Navbar.module.css"
 import images from '../../assets'
-import {Modal, TokenList} from '../index'
+import { Modal, TokenList } from '../index'
 
 const Navbar = () => {
   const menuItems = [
@@ -29,44 +29,44 @@ const Navbar = () => {
     (
       <div className={Styles.navbar}>
         <div className={Styles.Navbar_Box}>
-        {/* logo */}
-        <div className={Styles.Navbar_Box_Left}>
-          <div className={Styles.Navbar_Box_Left_Logo}>
-            <Image src={images.logo} alt="logo" width={50} height={50} />
-          </div>
-          <div className={Styles.Navbar_Box_Left_Menu}>
-            {menuItems.map((item, index) => (
-           <Link key={index + 1} href={{pathname: `${item.name}, query: ${item.link}`}}>
-            <p className={Styles.Navbar_Box_Left_Menu_Item}>{item.name}</p>
-           </Link>
-                ))}
-          </div>
-        </div>
-        {/* middle */}
-        <div className={Styles.Navbar_Box_Middle}>
-          <div className={Styles.Navbar_Box_Middle_Search}>
-            <div className={Styles.Navbar_Box_Middle_Search_Img}>
-              <Image src={images.Search} alt="search" width={20} height={20} />
+          {/* logo */}
+          <div className={Styles.Navbar_Box_Left}>
+            <div className={Styles.Navbar_Box_Left_Logo}>
+              <Image src={images.logo} alt="logo" width={50} height={50} />
             </div>
-            <input type="text" placeholder="Search tokens and NFT collections" />
+            <div className={Styles.Navbar_Box_Left_Menu}>
+              {menuItems.map((item, index) => (
+                <Link key={index + 1} href={{ pathname: `${item.name}, query: ${item.link}` }}>
+                  <p className={Styles.Navbar_Box_Left_Menu_Item}>{item.name}</p>
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-        {/* right */}  
-        <div className={Styles.Navbar_Box_Right}>
-          <div className={Styles.Navbar_Box_Right_Box}>
-            <div className={Styles.Navbar_Box_Right_Box_Img}>
-              <Image src={images.ETH} alt="Ether" width={30} height={30} />
+          {/* middle */}
+          <div className={Styles.Navbar_Box_Middle}>
+            <div className={Styles.Navbar_Box_Middle_Search}>
+              <div className={Styles.Navbar_Box_Middle_Search_Img}>
+                <Image src={images.Search} alt="search" width={20} height={20} />
+              </div>
+              <input type="text" placeholder="Search tokens and NFT collections" />
+            </div>
+          </div>
+          {/* right */}
+          <div className={Styles.Navbar_Box_Right}>
+            <div className={Styles.Navbar_Box_Right_Box}>
+              <div className={Styles.Navbar_Box_Right_Box_Img}>
+                <Image src={images.ETH} alt="Ether" width={30} height={30} />
               </div>
               <p>Network Name</p>
+            </div>
+            <button onClick={() => { }}>Address</button>
+            {openModal && <Modal setOpenModal={setOpenModal} connectWallet="Connect" />}
           </div>
-          <button onClick={() => {}}>Address</button>
-          {openModal && <Modal setOpenModal={setOpenModal} connectWallet="Connect" />}
-        </div>
         </div>
 
-        // tokenlist component 
-        {openTokenBox && (<TokenList tokenData = "hey" TokenList setOpenTokenBox={setOpenTokenBox} />)}
-        </div>
+        {/* tokenlist component  */}
+        {openTokenBox && (<TokenList tokenData="hey" TokenList setOpenTokenBox={setOpenTokenBox} />)}
+      </div>
     )
   )
 }
